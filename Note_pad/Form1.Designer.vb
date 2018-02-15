455d2e022d319cf31d396b222e08099a9995ee0f
Jay Buddhdev <jaybuddhdev99@hotmail.com>
2018-02-15 17:48:19 +0530

    Add project files.

---
+++ Note_pad/Form1.Designer.vb
@@ -0,0 +1,321 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Rtd_bx = New System.Windows.Forms.RichTextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.PrintToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExportToPdfToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UndoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem3 = New System.Windows.Forms.ToolStripSeparator()
+        Me.CutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CopyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PasteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DeleteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem4 = New System.Windows.Forms.ToolStripSeparator()
+        Me.FindToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReplaceToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem5 = New System.Windows.Forms.ToolStripSeparator()
+        Me.SelectAllToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TimeAndDateToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FormatToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.WordWrapToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FontToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.FontDialog1 = New System.Windows.Forms.FontDialog()
+        Me.PrintDialog1 = New System.Windows.Forms.PrintDialog()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.SaveFileDialog2 = New System.Windows.Forms.SaveFileDialog()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Rtd_bx
+        '
+        Me.Rtd_bx.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Rtd_bx.Location = New System.Drawing.Point(0, 28)
+        Me.Rtd_bx.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Rtd_bx.Name = "Rtd_bx"
+        Me.Rtd_bx.Size = New System.Drawing.Size(635, 317)
+        Me.Rtd_bx.TabIndex = 0
+        Me.Rtd_bx.Text = ""
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.FormatToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(5, 2, 0, 2)
+        Me.MenuStrip1.Size = New System.Drawing.Size(635, 28)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.OpenToolStripMenuItem, Me.SaveToolStripMenuItem, Me.ToolStripMenuItem1, Me.PrintToolStripMenuItem, Me.ExportToPdfToolStripMenuItem, Me.ToolStripMenuItem2, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(44, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.N), System.Windows.Forms.Keys)
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(173, 26)
+        Me.NewToolStripMenuItem.Text = "New"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.O), System.Windows.Forms.Keys)
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(173, 26)
+        Me.OpenToolStripMenuItem.Text = "Open"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(173, 26)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(170, 6)
+        '
+        'PrintToolStripMenuItem
+        '
+        Me.PrintToolStripMenuItem.Name = "PrintToolStripMenuItem"
+        Me.PrintToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.P), System.Windows.Forms.Keys)
+        Me.PrintToolStripMenuItem.Size = New System.Drawing.Size(173, 26)
+        Me.PrintToolStripMenuItem.Text = "Print"
+        '
+        'ExportToPdfToolStripMenuItem
+        '
+        Me.ExportToPdfToolStripMenuItem.Name = "ExportToPdfToolStripMenuItem"
+        Me.ExportToPdfToolStripMenuItem.Size = New System.Drawing.Size(173, 26)
+        Me.ExportToPdfToolStripMenuItem.Text = "Export To Pdf"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(170, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(173, 26)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.UndoToolStripMenuItem, Me.ToolStripMenuItem3, Me.CutToolStripMenuItem, Me.CopyToolStripMenuItem, Me.PasteToolStripMenuItem, Me.DeleteToolStripMenuItem, Me.ToolStripMenuItem4, Me.FindToolStripMenuItem, Me.ReplaceToolStripMenuItem, Me.ToolStripMenuItem5, Me.SelectAllToolStripMenuItem, Me.TimeAndDateToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(47, 24)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'UndoToolStripMenuItem
+        '
+        Me.UndoToolStripMenuItem.Enabled = False
+        Me.UndoToolStripMenuItem.Name = "UndoToolStripMenuItem"
+        Me.UndoToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Z), System.Windows.Forms.Keys)
+        Me.UndoToolStripMenuItem.Size = New System.Drawing.Size(208, 26)
+        Me.UndoToolStripMenuItem.Text = "Undo"
+        '
+        'ToolStripMenuItem3
+        '
+        Me.ToolStripMenuItem3.Name = "ToolStripMenuItem3"
+        Me.ToolStripMenuItem3.Size = New System.Drawing.Size(205, 6)
+        '
+        'CutToolStripMenuItem
+        '
+        Me.CutToolStripMenuItem.Enabled = False
+        Me.CutToolStripMenuItem.Name = "CutToolStripMenuItem"
+        Me.CutToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
+        Me.CutToolStripMenuItem.Size = New System.Drawing.Size(208, 26)
+        Me.CutToolStripMenuItem.Text = "Cut"
+        '
+        'CopyToolStripMenuItem
+        '
+        Me.CopyToolStripMenuItem.Enabled = False
+        Me.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        Me.CopyToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
+        Me.CopyToolStripMenuItem.Size = New System.Drawing.Size(208, 26)
+        Me.CopyToolStripMenuItem.Text = "Copy"
+        '
+        'PasteToolStripMenuItem
+        '
+        Me.PasteToolStripMenuItem.Name = "PasteToolStripMenuItem"
+        Me.PasteToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.V), System.Windows.Forms.Keys)
+        Me.PasteToolStripMenuItem.Size = New System.Drawing.Size(208, 26)
+        Me.PasteToolStripMenuItem.Text = "Paste"
+        '
+        'DeleteToolStripMenuItem
+        '
+        Me.DeleteToolStripMenuItem.Enabled = False
+        Me.DeleteToolStripMenuItem.Name = "DeleteToolStripMenuItem"
+        Me.DeleteToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.Delete
+        Me.DeleteToolStripMenuItem.Size = New System.Drawing.Size(208, 26)
+        Me.DeleteToolStripMenuItem.Text = "Delete"
+        '
+        'ToolStripMenuItem4
+        '
+        Me.ToolStripMenuItem4.Name = "ToolStripMenuItem4"
+        Me.ToolStripMenuItem4.Size = New System.Drawing.Size(205, 6)
+        '
+        'FindToolStripMenuItem
+        '
+        Me.FindToolStripMenuItem.Enabled = False
+        Me.FindToolStripMenuItem.Name = "FindToolStripMenuItem"
+        Me.FindToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.F), System.Windows.Forms.Keys)
+        Me.FindToolStripMenuItem.Size = New System.Drawing.Size(208, 26)
+        Me.FindToolStripMenuItem.Text = "Find"
+        '
+        'ReplaceToolStripMenuItem
+        '
+        Me.ReplaceToolStripMenuItem.Enabled = False
+        Me.ReplaceToolStripMenuItem.Name = "ReplaceToolStripMenuItem"
+        Me.ReplaceToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.H), System.Windows.Forms.Keys)
+        Me.ReplaceToolStripMenuItem.Size = New System.Drawing.Size(208, 26)
+        Me.ReplaceToolStripMenuItem.Text = "Replace"
+        '
+        'ToolStripMenuItem5
+        '
+        Me.ToolStripMenuItem5.Name = "ToolStripMenuItem5"
+        Me.ToolStripMenuItem5.Size = New System.Drawing.Size(205, 6)
+        '
+        'SelectAllToolStripMenuItem
+        '
+        Me.SelectAllToolStripMenuItem.Enabled = False
+        Me.SelectAllToolStripMenuItem.Name = "SelectAllToolStripMenuItem"
+        Me.SelectAllToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.A), System.Windows.Forms.Keys)
+        Me.SelectAllToolStripMenuItem.Size = New System.Drawing.Size(208, 26)
+        Me.SelectAllToolStripMenuItem.Text = "Select All"
+        '
+        'TimeAndDateToolStripMenuItem
+        '
+        Me.TimeAndDateToolStripMenuItem.Name = "TimeAndDateToolStripMenuItem"
+        Me.TimeAndDateToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F5
+        Me.TimeAndDateToolStripMenuItem.Size = New System.Drawing.Size(208, 26)
+        Me.TimeAndDateToolStripMenuItem.Text = "Time And Date"
+        '
+        'FormatToolStripMenuItem
+        '
+        Me.FormatToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.WordWrapToolStripMenuItem, Me.FontToolStripMenuItem})
+        Me.FormatToolStripMenuItem.Name = "FormatToolStripMenuItem"
+        Me.FormatToolStripMenuItem.Size = New System.Drawing.Size(68, 24)
+        Me.FormatToolStripMenuItem.Text = "Format"
+        '
+        'WordWrapToolStripMenuItem
+        '
+        Me.WordWrapToolStripMenuItem.CheckOnClick = True
+        Me.WordWrapToolStripMenuItem.Name = "WordWrapToolStripMenuItem"
+        Me.WordWrapToolStripMenuItem.Size = New System.Drawing.Size(160, 26)
+        Me.WordWrapToolStripMenuItem.Text = "Word Wrap"
+        '
+        'FontToolStripMenuItem
+        '
+        Me.FontToolStripMenuItem.Name = "FontToolStripMenuItem"
+        Me.FontToolStripMenuItem.Size = New System.Drawing.Size(160, 26)
+        Me.FontToolStripMenuItem.Text = "Font.."
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.DefaultExt = "txt"
+        Me.SaveFileDialog1.Filter = """TXT files|*.txt|All Files|*.*|Doc|*.doc"""
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.Filter = """TXT files|*.txt|All Files|*.*"""
+        Me.OpenFileDialog1.ValidateNames = False
+        '
+        'PrintDialog1
+        '
+        Me.PrintDialog1.UseEXDialog = True
+        '
+        'SaveFileDialog2
+        '
+        Me.SaveFileDialog2.DefaultExt = "pdf"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(635, 345)
+        Me.Controls.Add(Me.Rtd_bx)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Rtd_bx As RichTextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
+    Friend WithEvents PrintToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UndoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem3 As ToolStripSeparator
+    Friend WithEvents CutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PasteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DeleteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem4 As ToolStripSeparator
+    Friend WithEvents FindToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem5 As ToolStripSeparator
+    Friend WithEvents SelectAllToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TimeAndDateToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FormatToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents WordWrapToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FontToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents FontDialog1 As FontDialog
+    Friend WithEvents PrintDialog1 As PrintDialog
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents ExportToPdfToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveFileDialog2 As SaveFileDialog
+    Friend WithEvents ReplaceToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Note_pad/Form1.vb
@@ -0,0 +1,226 @@
+﻿Imports System
+Imports System.IO
+Imports iTextSharp
+Imports iTextSharp.text.pdf
+Imports iTextSharp.text
+
+
+Public Class Form1
+    Private Sub TimeAndDateToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TimeAndDateToolStripMenuItem.Click
+        Rtd_bx.Text = System.DateTime.Now.ToString
+
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+
+        If Rtd_bx.Modified Then
+
+            Dim ask As MsgBoxResult
+            ask = MsgBox("Do you want to save the changes", MsgBoxStyle.YesNoCancel, "New Document")
+            If ask = MsgBoxResult.No Then
+                Rtd_bx.Clear()
+            ElseIf ask = MsgBoxResult.Cancel Then
+            ElseIf ask = MsgBoxResult.Yes Then
+                SaveFileDialog1.ShowDialog()
+                My.Computer.FileSystem.WriteAllText(SaveFileDialog1.FileName, Rtd_bx.Text, False)
+                Rtd_bx.Clear()
+            End If
+
+        Else
+            Rtd_bx.Clear()
+        End If
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+
+        If Rtd_bx.Modified Then
+
+            Dim ask As MsgBoxResult
+            ask = MsgBox("Do you want to save the changes", MsgBoxStyle.YesNoCancel, "Open Document")
+            If ask = MsgBoxResult.No Then
+                OpenFileDialog1.ShowDialog()
+                Rtd_bx.Text = My.Computer.FileSystem.ReadAllText(OpenFileDialog1.FileName)
+            ElseIf ask = MsgBoxResult.Cancel Then
+            ElseIf ask = MsgBoxResult.Yes Then
+                SaveFileDialog1.ShowDialog()
+                My.Computer.FileSystem.WriteAllText(SaveFileDialog1.FileName, Rtd_bx.Text, False)
+                Rtd_bx.Clear()
+            End If
+        Else
+
+            OpenFileDialog1.ShowDialog()
+            Try
+                Rtd_bx.Text = My.Computer.FileSystem.ReadAllText(OpenFileDialog1.FileName)
+            Catch ex As Exception
+            End Try
+        End If
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+
+        SaveFileDialog1.ShowDialog()
+
+        If My.Computer.FileSystem.FileExists(SaveFileDialog1.FileName) Then
+            Dim ask As MsgBoxResult
+            ask = MsgBox("File already exists, would you like to replace it?", MsgBoxStyle.YesNo, "File Exists")
+
+            If ask = MsgBoxResult.No Then
+                SaveFileDialog1.ShowDialog()
+
+            ElseIf ask = MsgBoxResult.Yes Then
+                My.Computer.FileSystem.WriteAllText(SaveFileDialog1.FileName, Rtd_bx.Text, False)
+            End If
+
+        Else
+            Try
+                My.Computer.FileSystem.WriteAllText(SaveFileDialog1.FileName, Rtd_bx.Text, False)
+            Catch ex As Exception
+            End Try
+        End If
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub UndoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UndoToolStripMenuItem.Click
+        If Rtd_bx.CanUndo Then
+            Rtd_bx.Undo()
+        Else
+        End If
+    End Sub
+
+    Private Sub CutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CutToolStripMenuItem.Click
+        My.Computer.Clipboard.Clear()
+        If Rtd_bx.SelectionLength > 0 Then
+            My.Computer.Clipboard.SetText(Rtd_bx.SelectedText)
+
+        End If
+        Rtd_bx.SelectedText = ""
+    End Sub
+
+    Private Sub CopyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem.Click
+        My.Computer.Clipboard.Clear()
+        If Rtd_bx.SelectionLength > 0 Then
+            My.Computer.Clipboard.SetText(Rtd_bx.SelectedText)
+
+        End If
+    End Sub
+
+    Private Sub PasteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PasteToolStripMenuItem.Click
+        If My.Computer.Clipboard.ContainsText Then
+            Rtd_bx.Paste()
+        End If
+    End Sub
+
+    Private Sub SelectAllToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SelectAllToolStripMenuItem.Click
+        Rtd_bx.SelectAll()
+    End Sub
+
+    Private Sub FindToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FindToolStripMenuItem.Click
+        Dim a As String
+        Dim b As String
+        a = InputBox("Enter text to be found")
+        b = InStr(Rtd_bx.Text, a)
+        If b Then
+            Rtd_bx.Focus()
+            Rtd_bx.SelectionStart = b - 1
+            Rtd_bx.SelectionLength = Len(a)
+        Else
+            MsgBox("Text not found.")
+        End If
+    End Sub
+
+    Private Sub ReplaceToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ReplaceToolStripMenuItem.Click
+        Dim a, b, c As String
+        a = InputBox("Enter Text To Be Replaced")
+        c = InputBox("Enter Text To Replace")
+        b = InStr(Rtd_bx.Text, a)
+        If b Then
+            Rtd_bx.Focus()
+            Rtd_bx.SelectionStart = b - 1
+            Rtd_bx.SelectionLength = Len(a)
+            Rtd_bx.SelectedText = c
+        Else
+            MsgBox("Text not found.")
+        End If
+    End Sub
+
+    Private Sub DeleteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DeleteToolStripMenuItem.Click
+
+        Rtd_bx.Clear()
+
+
+    End Sub
+
+    Private Sub FontToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FontToolStripMenuItem.Click
+        FontDialog1.ShowDialog()
+        Rtd_bx.Font = FontDialog1.Font
+    End Sub
+
+    Private Sub PrintToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PrintToolStripMenuItem.Click
+        PrintDialog1.Document = PrintDocument1
+        PrintDialog1.PrinterSettings = PrintDocument1.PrinterSettings
+        PrintDialog1.AllowSomePages = True
+        If PrintDialog1.ShowDialog = DialogResult.OK Then
+            PrintDocument1.PrinterSettings = PrintDialog1.PrinterSettings
+            PrintDocument1.Print()
+        End If
+    End Sub
+
+    Private Sub WordWrapToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles WordWrapToolStripMenuItem.Click
+
+        If Rtd_bx.WordWrap = True Then
+            Rtd_bx.WordWrap = False
+        ElseIf Rtd_bx.WordWrap = False Then
+            Rtd_bx.WordWrap = True
+        End If
+    End Sub
+
+    Private Sub ExportToPdfToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExportToPdfToolStripMenuItem.Click
+        If Rtd_bx.Text <> vbNullString Then
+            SaveFileDialog2.ShowDialog()
+            Dim pdfdoc As New Document
+            Try
+                Dim pdfwrite As PdfWriter = PdfWriter.GetInstance(pdfdoc, New FileStream(SaveFileDialog2.FileName, FileMode.Create))
+                MessageBox.Show("PDF Created Successfully.")
+            Catch ex As Exception
+            End Try
+            pdfdoc.Open()
+            pdfdoc.Add(New Paragraph(Rtd_bx.Text))
+            pdfdoc.Close()
+
+        Else
+            MessageBox.Show("Please Write Something")
+        End If
+
+
+    End Sub
+
+    Private Sub Rtd_bx_TextChanged(sender As Object, e As EventArgs) Handles Rtd_bx.TextChanged
+        Rtd_bx.WordWrap = False
+
+        If Rtd_bx.Text = vbNullString Then
+            UndoToolStripMenuItem.Enabled = False
+            CutToolStripMenuItem.Enabled = False
+            CopyToolStripMenuItem.Enabled = False
+
+            SelectAllToolStripMenuItem.Enabled = False
+            ReplaceToolStripMenuItem.Enabled = False
+            FindToolStripMenuItem.Enabled = False
+            DeleteToolStripMenuItem.Enabled = False
+        ElseIf Rtd_bx.Text <> vbNullString Then
+            UndoToolStripMenuItem.Enabled = True
+            CutToolStripMenuItem.Enabled = True
+            CopyToolStripMenuItem.Enabled = True
+
+            SelectAllToolStripMenuItem.Enabled = True
+            ReplaceToolStripMenuItem.Enabled = True
+            FindToolStripMenuItem.Enabled = True
+            DeleteToolStripMenuItem.Enabled = True
+        End If
+    End Sub
+
+
+End Class
